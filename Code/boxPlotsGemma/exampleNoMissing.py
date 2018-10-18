c2422ee4fde9facdd15ef908a242073b2633387f
Gemma Martini <gg.gemmina93@gmail.com>
2018-10-18 18:41:55 +0200

    Added flag to pass to printPlots a dataframe. New file exampleNoMissing.py created to generate boxplots from the dataset without missing values.

--- Code/boxPlotsGemma/exampleNoMissing.py
+++ Code/boxPlotsGemma/exampleNoMissing.py
@@ -1,31 +1,34 @@
 import os
+import sys
+sys.path.insert(0, '../Riccardo')
 from plots import printPlots
+from MissingValues_2 import remove_missing_values
 import pandas as pd
 
 #Small dataset for head
 #filePath = "../../Dataset/credit_default_train_small.csv"
 
 filePath = "../../Dataset/credit_default_train.csv"
-newpath = "boxplots/"
+newpath = "boxplots/NoMissingValues/"
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 figExtension = "png"
 #figExtension = "pdf"
 #figExtension = "svg"
 
+cc = pd.read_csv(filePath)
+cc_NoMV = remove_missing_values(cc)
 #Example of boxplot of limit values
 limitNames = ["limit"]
 limitBox = newpath + "limit"
 howManyLimit = 1
-printPlots(limitBox, figExtension, filePath, howManyLimit, limitNames)
-cc = pd.read_csv(filePath)
 printPlots(limitBox, figExtension, cc, howManyLimit, limitNames, None, 1)
 
 #Example of boxplot of limit values last flag activated
 limitNames = ["limit"]
 limitBox = newpath + "limitS"
 howManyLimit = 1
-printPlots(limitBox, figExtension, filePath, howManyLimit, limitNames, 1)
+printPlots(limitBox, figExtension, cc, howManyLimit, limitNames, 1, 1)
 #Use flag 1 at the end (default none) to obtain the limit boxplot as
 #function of the average income of Taiwan (NT$49,989, about US$1,700)
 
@@ -33,22 +36,22 @@ printPlots(limitBox, figExtension, filePath, howManyLimit, limitNames, 1)
 ageNames = ["age"]
 ageBox = newpath + "age"
 howManyAge = 1
-printPlots(ageBox, figExtension, filePath, howManyAge, ageNames)
+printPlots(ageBox, figExtension, cc, howManyAge, ageNames, None, 1)
 
 #Example of boxplot of ps values
 psNames = ["ps-sep", "ps-aug", "ps-jul", "ps-jun", "ps-may", "ps-apr"]
 psBox = newpath + "ps"
 howManyPs = 6
-printPlots(psBox, figExtension, filePath, howManyPs, psNames)
+printPlots(psBox, figExtension, cc, howManyPs, psNames, None, 1)
 
 #Example of boxplot of ba values
 baNames = ["ba-sep", "ba-aug", "ba-jul", "ba-jun", "ba-may", "ba-apr"]
 baBox = newpath + "ba"
 howManyBa = 6
-printPlots(baBox, figExtension, filePath, howManyBa, baNames)
+printPlots(baBox, figExtension, cc, howManyBa, baNames, None, 1)
 
 #Example of boxplot of pa values
 paNames = ["pa-sep", "pa-aug", "pa-jul", "pa-jun", "pa-may", "pa-apr"]
 paBox = newpath + "pa"
 howManyPa = 6
-printPlots(paBox, figExtension, filePath, howManyPa, paNames)
+printPlots(paBox, figExtension, cc, howManyPa, paNames, None, 1)
--- Code/boxPlotsGemma/plots.py
+++ Code/boxPlotsGemma/plots.py
@@ -4,10 +4,11 @@ import numpy as np
 import matplotlib as mpl
 mpl.use('agg')
 import matplotlib.pyplot as plt
+import pandas as pd
 """This function takes as an input the path of the file that represents the dataset (filePath) and the string which is the name of a column (colName). This function returns the column as a list or -1, if thecolumn doesn't have numerical values"""
 
 
-def highlightColumn(filePath, colName):
+def highlightColumn(filePath, colName, pandas=None):
     if ((colName == "sex") | (colName == "education") | (colName == "status") |
         (colName == "credit_default")):
         print("Cannot print boxplot from non numerical data\n")
@@ -23,61 +24,71 @@ def highlightColumn(filePath, colName):
         print("Typo in the column name\n")
         return -1
     else:
-        data = open(filePath, "r")
-        if (colName == "limit"):
-            col = 0
-        if (colName == "age"):
-            col = 4
-        if (colName == "ps-sep"):
-            col = 5
-        if (colName == "ps-aug"):
-            col = 6
-        if (colName == "ps-jul"):
-            col = 7
-        if (colName == "ps-jun"):
-            col = 8
-        if (colName == "ps-may"):
-            col = 9
-        if (colName == "ps-apr"):
-            col = 10
-        if (colName == "ba-sep"):
-            col = 11
-        if (colName == "ba-aug"):
-            col = 12
-        if (colName == "ba-jul"):
-            col = 13
-        if (colName == "ba-jun"):
-            col = 14
-        if (colName == "ba-may"):
-            col = 15
-        if (colName == "ba-apr"):
-            col = 16
-        if (colName == "pa-sep"):
-            col = 17
-        if (colName == "pa-aug"):
-            col = 18
-        if (colName == "pa-jul"):
-            col = 19
-        if (colName == "pa-jun"):
-            col = 20
-        if (colName == "pa-may"):
-            col = 21
-        if (colName == "pa-apr"):
-            col = 22
-    lines = [list(l.split(',')) for l in data]
-    selectedCol = []
-    #print("Colonna ", colName)
-    for i in range(1, len(lines)):
-        #print("colonna vera: ", lines[i][col])
-        selectedCol.append(int(lines[i][col]))
-        #print(selectedCol[i - 1])
+        if (pandas):
+            data = filePath
+            return getattr(data, colName)
+        else:
+            data = open(filePath, "r")
+            if (colName == "limit"):
+                col = 0
+            if (colName == "age"):
+                col = 4
+            if (colName == "ps-sep"):
+                col = 5
+            if (colName == "ps-aug"):
+                col = 6
+            if (colName == "ps-jul"):
+                col = 7
+            if (colName == "ps-jun"):
+                col = 8
+            if (colName == "ps-may"):
+                col = 9
+            if (colName == "ps-apr"):
+                col = 10
+            if (colName == "ba-sep"):
+                col = 11
+            if (colName == "ba-aug"):
+                col = 12
+            if (colName == "ba-jul"):
+                col = 13
+            if (colName == "ba-jun"):
+                col = 14
+            if (colName == "ba-may"):
+                col = 15
+            if (colName == "ba-apr"):
+                col = 16
+            if (colName == "pa-sep"):
+                col = 17
+            if (colName == "pa-aug"):
+                col = 18
+            if (colName == "pa-jul"):
+                col = 19
+            if (colName == "pa-jun"):
+                col = 20
+            if (colName == "pa-may"):
+                col = 21
+            if (colName == "pa-apr"):
+                col = 22
+            lines = [list(l.split(',')) for l in data]
+            selectedCol = []
+            #print("Colonna ", colName)
+            for i in range(1, len(lines)):
+                #print("colonna vera: ", lines[i][col])
+                selectedCol.append(int(lines[i][col]))
+                #print(selectedCol[i - 1])
     return selectedCol
 
 
 """This function creates and saves a boxplot as "boxName.figExtension", which has howMany different plots (whose names are in the string list called colNames) from the file whose path is filePath"""
 
 
-def printPlots(boxName, figExtension, filePath, howMany, colNames, flag=None):
+def printPlots(boxName,
+               figExtension,
+               filePath,
+               howMany,
+               colNames,
+               flag=None,
+               pandas=None):
     if howMany == 0:
         return
     dataToPlot = []
@@ -85,7 +96,7 @@ def printPlots(boxName, figExtension, filePath, howMany, colNames, flag=None):
     xNames = []
     for i in range(0, howMany):
         colName = colNames[i]
-        currCol = highlightColumn(filePath, colName)
+        currCol = highlightColumn(filePath, colName, pandas)
         if flag:
             if colName == "limit":
                 myInt = 49989
